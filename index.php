439a3c127fd31d1c1958f5513ec8a59f6eec15d5
maves9 <mvmartinxd@gmail.com>
2017-08-21 19:40:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,168 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="Vi er en kombineret øl- og vinbar med små tapasretter på menukortet der leveres af Humleland, VinDanmark og CANblau.">
+<meta name="keywords" content="Hantwerk, hantwerk, Aarhus, århus, øl, mad, vin, canblau, vindanmark, humleland">
+<meta name="author" content="Hantwerk">
+<meta name="title" content="Hantwerk">
+  
+<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+<title>Hantwerk | Hjem</title>
+
+</head>
+
+<body>
+
+<!-- BAGGRUNDSBILLEDE -->
+<main id="bgImg">
+
+<!--PHP INCLUDE AF NAVIGATIONEN -->
+<nav class="navibar">
+    <ul>
+        <li><a id="menuSelected" class="naviLinks" href="menu.php">Menu</a></li>
+        <li><a id="konceptSelected" class="naviLinks" href="koncept.php">Koncept</a></li>
+        <li><a  href="index.php"><img id="logo" src="images/LOGO_HW_emblem.png" alt="Hantwerk logo"/></a></li>
+        <li><a class="naviLinks" href="https://book.dinnerbooking.com/dk/da-DK/book/index/1260/2">Book</a></li>
+        <li><a id="contactSelected" class="naviLinks" href="contact.php">Kontakt</a></li>
+    </ul>
+</nav>
+
+<!-- TEKSTBOX PÅ FORSIDEN -->
+<figure id="textBox">
+  <h3>Velkommen til Hantwerk!</h3>
+  <p>Vi er en kombineret øl- og vinbar med et varierende udvalg af øl og vine, som leveres af Humleland og VinDanmark. Du kan også få lækre tapasretter leveret af CANblau EN EL MUELLE.<br>
+    Læs mere <a id="boldTag" href="koncept.php">her</a></p>
+  
+  <!-- JQUERY SLIDES AF TEKST -->
+  <p class="quotes">torsdag 16.00 - 00.00</p>
+  <p class="quotes">fredag 14.00 - 00.00</p>
+  <p class="quotes">lørdag 12.00 - 00.00</p>
+</figure>
+
+
+<img id="mobileLogo" src="images/LOGO_HW_emblem.png" alt="Hantwerk logo"/>
+
+<h1>Hantwerk</h1>
+<p>Vi er en kombineret øl- og vinbar med små tapasretter på menukortet der leveres af Humleland, VinDanmark og CANblau.</p>
+
+<hr>
+<h2>Ølkort</h2>
+
+<p>Nelson Waiti</p>
+                  <span class="subtekst">(pale ale 5.6%)</span></p>
+            <p>Blondinens Kusine<br>
+                  <span class="subtekst">(belgisk blond ale 5.6%)</span></p>
+            <p>Equinox Mosaic<br>
+                  <span class="subtekst">(dobbelt IPA 8.0%)</span></p>
+            <p>Hvidt<br>
+                  <span class="subtekst">(belgisk wit 5.0%)</span></p>
+            <p>Den Lige Vej<br>
+                  <span class="subtekst">(engelsk golden ale 4.2%)</span></p>
+            <p>Den Falske Fætter<br>
+                  <span class="subtekst">(mørk wit 4.4%)</span></p>
+            <p>Det Gode Liv<br>
+                  <span class="subtekst">(pale ale 4.6%)</span></p>
+
+<p>En Tur i Høet</p>
+                  <span class="subtekst">(amr. golden ale 4.5%)</span></p>
+            <p>Farmands Datter<br>
+                  <span class="subtekst">(blond 4.2%)</span></p>
+            <p>Blondinen<br>
+                  <span class="subtekst">(belgisk blond 3.7%)</span></p>
+            <p>Det’ Jo Helt Sort<br>
+                  <span class="subtekst">(IPA 9.0%)</span></p>
+            <p>Postmanden<br>
+                  <span class="subtekst">(IPA 6.7%)</span></p>
+            <p>Fanø<br>
+                  <span class="subtekst">(imp lynghvede 9.7%)</span></p>
+            <p>Fanø 1741<br>
+                  <span class="subtekst">(bierre de gode 7.4%)</span></p>
+
+
+<hr>
+<h2>Vinkort</h2>
+ <p> Lothian Riesling, frisk afrikaner<br>
+                  <span class="subtekst">(75,-/300,-)</span></p>
+            <p>Kathrin Wind, pinot madeleine<br>
+                  <span class="subtekst">(75,-/125,-/450,-)</span></p>
+            <p>1+1=3, spansk og bobler<br>
+                  <span class="subtekst">(65,-/300,-)</span></p>
+            <p>Levitation, 95PP<br>
+                  <span class="subtekst">(80,-/130,-/500,-)</span></p>
+            <p>Susumaniello, fed italiener<br>
+                  <span class="subtekst">(80,-/130,-)</span></p>
+            <p>Horny Owl, kåd Shiraz<br>
+                  <span class="subtekst">(65,-/260,-)</span></p>
+            <p>Lothian Chardonay, cremet afrikaner<br>
+                  <span class="subtekst">(75,-/300,-)</span></p>
+                  
+                  <hr>
+                  <h2>Menukort</h2>
+                  <p>Naf-Naf planken (til 2 pers.)<span class="subtekst">
+              (200,-)</span></p>
+            <p>Ensalada Folie (skøe salat)<span class="subtekst">
+              (100,-)</span></p>
+            <p>3 pinxo’s<span class="subtekst">
+              (80,-)</span></p>
+            <p>Estrella Fugaz (stjerneskud)<span class="subtekst">
+              (160,-)</span></p>
+            <p>Mini burger med fritter og baconmayo<span class="subtekst">
+              (80,-)</span></p>
+            <p>Stifado de carne (dagens kødret)<span class="subtekst">
+              (100,-)</span></p>
+            <p>Havnens fangst med garni og salsa<span class="subtekst">
+              (200,-)</span></p>
+
+<hr>
+<h1>Kontakt</h1>
+<p>Torsdag</p>
+<p>Fredag</p>
+<p>Lørdag</p>
+<p>Søn - Ons</p>
+
+<p>16.00 - 00.00</p>
+<p>14.00 - 00.00</p>
+<p>12.00 - 00.00</p>
+<p>Lukket</p>
+
+<address>
+<p>8000 Aarhus C</p>
+      <p>Fiskerivej 2</p>
+      <p>91 25 62 57</p>
+      <p>selskab@hantwerk.dk</p>
+      <p>Cvr. 37201901</p>
+</address>
+
+
+
+<a id="bookbord" href="https://book.dinnerbooking.com/dk/da-DK/book/index/1260/2">Bestil bord</a>
+
+
+
+</main>
+
+<!-- JQUERY -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script> 
+<script>
+
+(function() {
+
+    var quotes = $(".quotes");
+    var quoteIndex = -1;
+    
+    function showNextQuote() {
+        ++quoteIndex;
+        quotes.eq(quoteIndex % quotes.length)
+            .fadeIn(1000)
+            .delay(1000)
+            .fadeOut(1000, showNextQuote);
+    }
+    
+    showNextQuote();
+    
+})();
+</script>
+</body>
+</html>
